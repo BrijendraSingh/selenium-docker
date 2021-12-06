c67d817846e2ad87c93bb95640b8722a275aa0b6
Brijendra Singh <brijendrasingh@Brijendras-MacBook-Pro.local>
2021-12-06 14:40:38 +0530

    tests updated

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -3,14 +3,20 @@ import setup.BaseTest;
 
 public class FirstTest extends BaseTest {
     @Test
-    public void gridTest(){
+    public void googleTest(){
         driver.get("https://google.com");
-        System.out.println("title-->"+ driver.getTitle());
+        System.out.println("title google-->"+ driver.getTitle());
     }
 
     @Test
-    public void gridTestTwo(){
+    public void facebookTest(){
         driver.get("https://facebook.com");
-        System.out.println("title-->"+ driver.getTitle());
+        System.out.println("title facebook-->"+ driver.getTitle());
+    }
+
+    @Test
+    public void twitterTest(){
+        driver.get("https://twitter.com");
+        System.out.println("title twitter-->"+ driver.getTitle());
     }
 }
